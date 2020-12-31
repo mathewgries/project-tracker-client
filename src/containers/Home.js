8a979a015696f31128abe13e61bb4ff4d36c6f86
mathewgries <mathewgries@gmail.com>
2020-12-30 22:13:04 -0500

    Setting up react app

---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Project Tracker</h1>
+            </div>
+        </div>
+    );
+}
